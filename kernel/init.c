b94538cd479877c128e0fbdc32ef9e0b8e308529
pine <2277367263@qq.com>
2024-12-25 13:41:38 +0800

    设置内核程序入口函数
    由于没有使用默认启动文件（如 crt0.o），因此未定义 main 函数也不会报错。
    （main 函数是由启动文件的代码调用的）
    
    在我们没有指定入口函数，链接器会将第一个函数当作入口函数
    
    为了不出错，这里使用 -e main 指定入口函数为 main

--- kernel/init.c
+++ kernel/init.c
@@ -1,31 +1,10 @@
 /* 内核测试代码 */
 
-#include "string.h"
-
-char *vmem = (char *)0xB8000;   // 显存指针
-
-void putchar(char c);
 void puts(char *s);
 
-void setupmain(void)
+int main(void)
 {
     puts("In Kernel Init!!!");
 
-    strlen("123");
-
     while(1);
-}
-
-void putchar(char c)
-{
-    *(vmem++) = c;
-    *(vmem++) = 0x9; // 高亮蓝色字符
-}
-
-void puts(char *s)
-{
-    while (*s)
-    {
-        putchar(*(s++));
-    }
 }

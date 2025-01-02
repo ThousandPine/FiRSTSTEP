6223393cb00be89ea9ffc9972cdc4709af07f9fe
pine <2277367263@qq.com>
2025-01-02 20:52:10 +0800

    修复栈初始化错误
    不能通过函数调用与提前返回的方式设置栈
    问题在于 GCC 编译出的代码：
    
          stack_init(stack + sizeof(stack));
    => 0x00100129 <+22>:	mov    eax,0x102400
       0x0010012e <+27>:	sub    esp,0xc
       0x00100131 <+30>:	push   eax
       0x00100132 <+31>:	call   0x1000fa <stack_init>
       0x00100137 <+36>:	add    esp,0x10
    
    在函数调用之前会对参数进行压栈，并且进行 16 字节对齐
    
       0x0010012e <+27>:	sub    esp,0xc
       0x00100131 <+30>:	push   eax
    
    而关键在于函数返回之后，它也会相应的增加 esp 的值，以释放栈空间
    
       0x00100137 <+36>:	add    esp,0x10
    
    因此当我们在函数内将 esp 设为我们想要的栈顶地址
    而函数返回时会执行语句增加 esp 的值
    此时 esp 的值就会大于我们之前设置的栈顶地址，也就发生了越界
    
    比如目前的情况就是 esp 的地址被初始化为 stack 变量末尾
    函数返回之后 esp 增加，越界到了 gdt 变量内
    调用函数时返回值等数据就被压栈到了 gdt 变量的范围里
    如果此时修改 gdt 的值，就会导致返回值等数据发送变化
    函数返回时就会跳转到错误的地址
    
    所以结论就是，esp 会在函数返回后被修改，因此不要把 esp 初始化放在可返回的函数内，而是直接放在不会返回的 main 函数中。

--- kernel/init.c
+++ kernel/init.c
@@ -6,32 +6,20 @@ void mem_init(void);
 
 static char stack[2048]; // 内核栈空间
 
-/**
- * 初始化内核栈
- *
- * 修改栈顶寄存器 esp 使其指向变量 stack 的空间
- * 内部包括 ret 指令，所以不能对该函数使用 inline
- */
-static __attribute__((noinline)) void stack_init(void *stack_top)
-{
-    // 设置新的栈会导致返回地址丢失，所以要提前记录
-    void *addr = __builtin_return_address(0);
-    asm volatile(
-        "mov %0, %%esp\n"           // 将新的栈指针设置到 esp 寄存器
-        "push %1\n"                 // 将函数返回地址压栈
-        "ret\n"                     // 需要提前返回，否则后续会执行 leave 恢复栈帧
-        :                           // 输出部分
-        : "r"(stack_top), "r"(addr) // 输入部分
-    );
-}
-
 int main(void)
 {
     /**
+     * 初始化内核栈
+     * 修改栈顶寄存器 esp 使其指向变量 stack 的空间
+     * 
      * 局部变量存储在栈空间
      * 所以初始化栈之前不得定义任何局部变量
      */
-    stack_init(stack + sizeof(stack));
+    asm volatile(
+        "mov %0, %%esp\n"            // 将新的栈指针设置到 esp 寄存器
+        :                            // 输出部分
+        : "r"(stack + sizeof(stack)) // 输入部分
+    );
 
     tty_init();
 

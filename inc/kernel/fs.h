7f6d35d5573aa2e3b0affa182f343f2aa9c18342
pine <2277367263@qq.com>
2025-01-07 12:51:53 +0800

    实现文件系统读取文件方法
    详情见注释
    
    ref:
    https://github.com/ThousandPine/FAT12_Test/blob/master/disk.c

--- inc/kernel/fs.h
+++ inc/kernel/fs.h
@@ -7,5 +7,5 @@ typedef struct File
     FatDirEntry fat_entry;
 } File;
 
-void fs_init(void);
 int file_open(const char *path, File *out_file);
+size_t file_read(void *dst, off_t offset, size_t size, File *file);
--- kernel/fs.c
+++ kernel/fs.c
@@ -3,12 +3,13 @@
 #include "kernel/mbr.h"
 #include "kernel/fat16.h"
 #include "kernel/kernel.h"
+#include "algobase.h"
+#include "string.h"
 
 #define FILENAME_MAX_LENGTH 12 // 文件名最大长度
 #define BLANK ' '              // 填充符号为空格
 #define PATH_SEPARATOR '/'     // 路径分隔符
 
-static uint8_t buf[SECT_SIZE];
 static PartitionEntry part = {0};
 static struct
 {
@@ -102,8 +103,10 @@ static int fat_name_cmp(const FatDirEntry *entry, const char *name)
  *
  * @param cluster 当前簇号（必须为合法值）
  */
-uint16_t fat_next_clus(uint16_t cluster)
+static uint16_t fat_next_clus(uint16_t cluster)
 {
+    static uint8_t buf[SECT_SIZE];
+
     // FAT 表的有效起始簇号是 0 ，所以不需要减 2
     uint32_t byte_offset = cluster * 2;
 
@@ -112,9 +115,19 @@ uint16_t fat_next_clus(uint16_t cluster)
 }
 
 // 检验 FAT16 簇号合法性
-uint8_t fat_check_clus(uint16_t cluster)
+static inline uint8_t fat_check_clus(uint16_t cluster)
 {
     return cluster > 0x1 && cluster < 0xFFF0;
+}
+
+// 将簇号转换为数据区对应的 LBA 地址
+static inline lba_t fat_clus2lba(uint16_t clus)
+{
+    /**
+     * 计算簇号对应的 LBA 地址
+     * 数据区有效簇号起始值为 2，所以要减去 2 再乘扇区数
+     */
+    return fat.data_start_lba + fat.bpb.sec_per_clus * (clus - 2);
 }
 
 /**
@@ -127,7 +140,10 @@ static lba_t fat_off2lba(const FatDirEntry *entry, off_t offset)
 {
     assert(offset % SECT_SIZE == 0);
 
-    // 找到所在的簇号
+    /**
+     * 找到 offset 所在的簇号
+     * 期间不断减小 offset 的值，将其转换为簇内偏移
+     */
     const size_t clus_size = fat.bpb.sec_per_clus * SECT_SIZE;
     uint16_t cur_clus = entry->fst_clus;
     while (offset >= clus_size)
@@ -141,12 +157,8 @@ static lba_t fat_off2lba(const FatDirEntry *entry, off_t offset)
         return 0;
     }
 
-    /**
-     * 计算簇号对应的 LBA 地址
-     * 数据区有效簇号起始值为 2，所以要减去 2 再乘扇区数
-     * 最后加上簇内 offset 的偏移扇区数
-     */
-    lba_t lba = fat.data_start_lba + fat.bpb.sec_per_clus * (cur_clus - 2) + offset / SECT_SIZE;
+    // 计算簇号对应的 LBA 地址, 再加上簇内偏移扇区数
+    lba_t lba = fat_clus2lba(cur_clus) + offset / SECT_SIZE;
     // 理论上来说文件内部 LBA 地址不可能为 0
     assert(lba > 0);
     return lba;
@@ -161,6 +173,8 @@ static lba_t fat_off2lba(const FatDirEntry *entry, off_t offset)
  */
 static int fat_find_entry(const char *path, FatDirEntry *out_entry)
 {
+    static uint8_t buf[SECT_SIZE];
+
     // 必须使用绝对路径
     assert(path[0] == PATH_SEPARATOR);
 
@@ -274,8 +288,171 @@ int file_open(const char *path, File *out_file)
     return fat_find_entry(path, &out_file->fat_entry);
 }
 
+/**
+ * 读取 FAT 文件存储的数据
+ *
+ * @param dst 数据保存位置
+ * @param offset 偏移字节，必须是扇区大小的整数倍
+ * @param size 读取字节数，必须是扇区大小的整数倍
+ * @param entry 文件条目
+ * @return 实际读取的字节数
+ */
+static size_t fat_read(void *dst, off_t offset, size_t size, const FatDirEntry *entry)
+{
+    assert(offset % SECT_SIZE == 0);
+    assert(size % SECT_SIZE == 0);
+
+    size_t read_bytes = 0;
+
+    /**
+     * 找到 offset 所在的簇号
+     * 期间不断减小 offset 的值，将其转换为簇内偏移
+     */
+    const size_t clus_size = fat.bpb.sec_per_clus * SECT_SIZE;
+    uint16_t cur_clus = entry->fst_clus;
+    while (offset >= clus_size)
+    {
+        cur_clus = fat_next_clus(cur_clus);
+        offset -= clus_size;
+    }
+
+    if (!fat_check_clus(cur_clus))
+    {
+        return read_bytes;
+    }
+
+    // 读取起始簇，要跳过簇内偏移的扇区
+    lba_t lba = fat_clus2lba(cur_clus) + offset / SECT_SIZE;
+    size_t read_size = MIN(clus_size - offset, size);
+    if (0 > ata_read(dst + read_bytes, lba, read_size / SECT_SIZE))
+    {
+        return read_bytes;
+    }
+    read_bytes += read_size;
+
+    // 读取中间的完整簇
+    while (size - read_bytes >= clus_size)
+    {
+        // 获取下一个簇号
+        cur_clus = fat_next_clus(cur_clus);
+        if (!fat_check_clus(cur_clus))
+        {
+            return read_bytes;
+        }
+        // 读取整个簇
+        lba = fat_clus2lba(cur_clus);
+        if (0 > ata_read(dst + read_bytes, lba, clus_size / SECT_SIZE))
+        {
+            return read_bytes;
+        }
+        read_bytes += clus_size;
+    }
+
+    // 读取结尾簇，仅读取前几个需要的扇区
+    if (read_bytes < size)
+    {
+        cur_clus = fat_next_clus(cur_clus);
+        if (!fat_check_clus(cur_clus))
+        {
+            return read_bytes;
+        }
+        lba = fat_clus2lba(cur_clus);
+        read_size = size - read_bytes;
+        if (0 > ata_read(dst + read_bytes, lba, read_size / SECT_SIZE))
+        {
+            return read_bytes;
+        }
+        read_bytes += read_size;
+    }
+
+    return read_bytes;
+}
+
+/**
+ * 读取文件
+ *
+ * @param dst 数据保存位置
+ * @param offset 偏移字节
+ * @param size 读取字节数
+ * @param file 文件信息
+ * @return 实际读取的字节数
+ */
+size_t file_read(void *dst, off_t offset, size_t size, File *file)
+{
+    // 不能超过文件上限
+    if (offset >= file->fat_entry.file_size)
+    {
+        return 0;
+    }
+    size = MIN(size, file->fat_entry.file_size - offset);
+
+    uint8_t buf[SECT_SIZE]; // 临时缓冲区
+    size_t read_bytes = 0;  // 已读取字节数
+
+    /**
+     * 之前的 API 都需要以扇区为单位进行读写
+     * 也就是说 offset 和 size 都必须是512的整数倍
+     * 为了实现自由读写功能
+     * 我们需要对开头和结尾的非扇区对齐部分进行特殊处理
+     * 先以扇区对齐的方式读出，再保留扇区内需要的部分
+     */
+
+    // 起始地址未对齐扇区，先读取属于该扇区内的数据
+    if (offset % SECT_SIZE != 0)
+    {
+        off_t sec_off = offset % SECT_SIZE;                // 扇区内偏移
+        size_t read_size = MIN(size, SECT_SIZE - sec_off); // 该扇区内要读取的数据大小
+
+        // 先读入临时缓冲区
+        size_t result = fat_read(buf, offset - sec_off, SECT_SIZE, &file->fat_entry);
+        if (result < SECT_SIZE)
+        {
+            return read_bytes;
+        }
+        // 将需要的部分拷贝出来
+        memcpy(dst, buf + sec_off, read_size);
+
+        read_bytes += read_size;
+    }
+
+    // 读取中间包含的完整扇区
+    if (size - read_bytes >= SECT_SIZE)
+    {
+        size_t read_size = ALIGN_DOWN(size - read_bytes, SECT_SIZE);
+
+        size_t result = fat_read(dst + read_bytes, offset + read_bytes, read_size, &file->fat_entry);
+        if (result < read_size)
+        {
+            return read_bytes + result;
+        }
+
+        read_bytes += read_size;
+    }
+
+    // 读取末尾非完整的扇区
+    if (read_bytes < size)
+    {
+        size_t read_size = size - read_bytes;
+
+        // 先读入临时缓冲区
+        size_t result = fat_read(buf, offset + read_bytes, SECT_SIZE, &file->fat_entry);
+        if (result < SECT_SIZE)
+        {
+            return read_bytes;
+        }
+        // 将需要的部分拷贝出来
+        memcpy(dst + read_bytes, buf, read_size);
+
+        read_bytes += read_size;
+    }
+
+    return read_bytes;
+}
+
 void fs_init(void)
 {
+    static uint8_t buf[SECT_SIZE];
+
     // 遍历 MBR 分区表，找到首个引导分区作为文件系统所在分区
     const MBR *mbr = (const MBR *)buf;
 

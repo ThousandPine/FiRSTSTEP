523e1b22a8b286a8274258bf8159ced51e28d14f
pine <2277367263@qq.com>
2025-01-10 18:19:51 +0800

    汇编实现内核入口
    将内核堆栈初始化放在汇编代码中实现
    
    之前在 C 函数中设置堆栈会有其他问题
    比如局部变量都是以一开始的 ESP 为基准创建的
    也就是局部变量还保存在旧堆栈中

--- kernel/init.c
+++ kernel/init.c
@@ -7,26 +7,8 @@ void fs_init(void);
 void idt_init(void);
 void pic_init(void);
 
-static char stack[2048]; // 内核栈空间
-
-int main(void)
+int init(void)
 {
-    // 关闭中断
-    cli();
-
-    /**
-     * 初始化内核栈
-     * 修改栈顶寄存器 esp 使其指向变量 stack 的空间
-     *
-     * 局部变量存储在栈空间
-     * 所以初始化栈之前不得定义任何局部变量
-     */
-    asm volatile(
-        "mov %0, %%esp\n"            // 将新的栈指针设置到 esp 寄存器
-        :                            // 输出部分
-        : "r"(stack + sizeof(stack)) // 输入部分
-    );
-
     tty_init();
 
     mem_init();

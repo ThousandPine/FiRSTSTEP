f7a37d5814b30efcf89b306b5d2eb02d7fe71e61
pine <2277367263@qq.com>
2025-01-12 21:41:22 +0800

    添加 TSS 初始化
    用于后续实现用户态到内核态的切换
    
    ref:
    https://wiki.osdev.org/Task_State_Segment
    https://wiki.osdev.org/Global_Descriptor_Table

--- inc/kernel/gdt.h
+++ inc/kernel/gdt.h
@@ -2,11 +2,12 @@
 
 #include "types.h"
 
-#define GDT_ENTRY_COUNT 5 // GDT 条目数量
+#define GDT_ENTRY_COUNT 6 // GDT 条目数量
 #define KER_CODE_INDEX 1  // GDT 内核代码段索引
 #define KER_DATA_INDEX 2  // GDT 内核数据段索引
 #define USER_CODE_INDEX 3 // GDT 用户代码段索引
 #define USER_DATA_INDEX 4 // GDT 用户数据段索引
+#define TSS_INDEX 5       // TSS 在 GDT 的索引
 
 #define DA_DR 0b1000  // 只读数据段
 #define DA_DRW 0b1001 // 可读写数据段
@@ -14,6 +15,8 @@
 #define DA_CR 0b1101  // 可读代码段
 #define DA_CC 0b1110  // 仅执行一致代码段
 #define DA_CCR 0b1111 // 可读一致代码段
+
+#define DA_TSS 0b0100 // 32-bit TSS (Available)
 
 // 段描述符，作为 GDT 或 LDT 的条目
 typedef struct SegmentDescriptor
@@ -51,5 +54,34 @@ typedef struct GDTDescriptor
  * @param ti 0 表示 GDT，1 表示 LDT
  */
 #define seg_sel_val(index, rpl, ti) (uint16_t)(((rpl) & 0x3) | (((ti) << 2) & 0x4) | (((index) << 3) & 0xFFF8))
+
+// TSS 结构
+typedef struct TSS
+{
+    uint32_t prev_tss; // 上一个任务的链接（在硬件任务切换中使用）
+    uint32_t esp0;     // 特权级 0 栈指针
+    uint16_t ss0;      // 特权级 0 栈段选择子
+    uint16_t reserved1;
+    uint32_t esp1; // 特权级 1 栈指针
+    uint16_t ss1;  // 特权级 1 栈段选择子
+    uint16_t reserved2;
+    uint32_t esp2; // 特权级 2 栈指针
+    uint16_t ss2;  // 特权级 2 栈段选择子
+    uint16_t reserved3;
+    uint32_t cr3;                                    // 页目录基地址
+    uint32_t eip;                                    // 指令指针
+    uint32_t eflags;                                 // 标志寄存器
+    uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi; // 通用寄存器
+    uint16_t es, reserved4;                          // 段寄存器
+    uint16_t cs, reserved5;
+    uint16_t ss, reserved6;
+    uint16_t ds, reserved7;
+    uint16_t fs, reserved8;
+    uint16_t gs, reserved9;
+    uint16_t ldt_selector; // LDT 选择子
+    uint16_t reserved10;
+    uint16_t debug_flag;  // 调试陷阱标志
+    uint16_t io_map_base; // I/O 权限位图基地址
+} __attribute__((packed)) TSS;
 
 void gdt_init(void);
--- kernel/gdt.c
+++ kernel/gdt.c
@@ -1,9 +1,10 @@
 #include "kernel/gdt.h"
 #include "string.h"
 
-static SegmentDescriptor gdt[GDT_ENTRY_COUNT];
+static SegmentDescriptor gdt[GDT_ENTRY_COUNT] = {0};
+static TSS tss = {0};
 
-static void set_gdt_entry(SegmentDescriptor *desc, uint32_t base, uint32_t limit, uint8_t dpl, uint8_t type)
+static void set_seg_descriptor(SegmentDescriptor *desc, uint32_t base, uint32_t limit, uint8_t dpl, uint8_t type)
 {
     desc->base_low = base & 0xFFFFFF;
     desc->base_hi = (base >> 24) & 0xFF;
@@ -18,13 +19,30 @@ static void set_gdt_entry(SegmentDescriptor *desc, uint32_t base, uint32_t limit
     desc->granularity = 1;
 }
 
+static void set_tss_descriptor(SegmentDescriptor *desc, uint32_t base, uint8_t dpl)
+{
+    uint32_t limit = sizeof(TSS) - 1;
+    desc->base_low = base & 0xFFFFFF;
+    desc->base_hi = (base >> 24) & 0xFF;
+    desc->lim_low = limit & 0xFFFF;
+    desc->lim_hi = (limit >> 16) & 0xF;
+    desc->dpl = dpl;
+    desc->type = DA_TSS;
+    desc->accessed = 1;
+    desc->present = 1;
+    desc->m32 = 1;
+    desc->long_mode = 0;
+    desc->granularity = 0;
+}
+
 void gdt_init(void)
 {
-    memset(gdt, 0, sizeof(*gdt));                                // NULL Segment
-    set_gdt_entry(gdt + KER_CODE_INDEX, 0, 0xFFFFF, 0, DA_CR);   // Kernel Code Segment
-    set_gdt_entry(gdt + KER_DATA_INDEX, 0, 0xFFFFF, 0, DA_DRW);  // Kernel Data Segment
-    set_gdt_entry(gdt + USER_CODE_INDEX, 0, 0xFFFFF, 3, DA_CR);  // User Code Segment
-    set_gdt_entry(gdt + USER_DATA_INDEX, 0, 0xFFFFF, 3, DA_DRW); // User Data Segment
+    memset(gdt, 0, sizeof(*gdt));                                     // NULL Segment
+    set_seg_descriptor(gdt + KER_CODE_INDEX, 0, 0xFFFFF, 0, DA_CR);   // Kernel Code Segment
+    set_seg_descriptor(gdt + KER_DATA_INDEX, 0, 0xFFFFF, 0, DA_DRW);  // Kernel Data Segment
+    set_seg_descriptor(gdt + USER_CODE_INDEX, 0, 0xFFFFF, 3, DA_CR);  // User Code Segment
+    set_seg_descriptor(gdt + USER_DATA_INDEX, 0, 0xFFFFF, 3, DA_DRW); // User Data Segment
+    set_tss_descriptor(gdt + TSS_INDEX, (uint32_t)&tss, 0);           // TSS
 
     // Set GDTR
     GDTDescriptor gdtr = {
@@ -47,4 +65,12 @@ void gdt_init(void)
           [data_seg] "i"(seg_sel_val(KER_DATA_INDEX, 0, 0)),
           [code_seg] "i"(seg_sel_val(KER_CODE_INDEX, 0, 0))
         : "memory", "ax");
+
+    // Set TR (TSS)
+    asm volatile(
+        "ltr %0\n"
+        :
+        : "r"(seg_sel_val(TSS_INDEX, 0, 0))
+        : "memory"
+    );
 }

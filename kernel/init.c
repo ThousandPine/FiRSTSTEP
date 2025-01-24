4ef21ad0c68f9b396cd6a4d8bc37f9f84019db35
pine <2277367263@qq.com>
2025-01-24 13:00:06 +0800

    删除测试代码

--- kernel/init.c
+++ kernel/init.c
@@ -1,14 +1,10 @@
 #include "kernel/kernel.h"
-#include "kernel/x86.h"
 
 void tty_init(void);
 void mem_init(void);
 void fs_init(void);
 void idt_init(void);
 void pic_init(void);
-
-#include "kernel/fs.h"
-#include "kernel/elf.h"
 
 int init(void)
 {
@@ -20,12 +16,6 @@ int init(void)
     pic_init();
 
     fs_init();
-
-    // 测试
-    int elf_loader(File * file);
-    File file;
-    file_open("/kernel", &file);
-    elf_loader(&file);
 
     // 内核不能 return，并且返回地址已经在初始化栈时丢失了
     panic("The kernel runs to the end");
